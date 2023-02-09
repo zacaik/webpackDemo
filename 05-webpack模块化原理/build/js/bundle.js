49e6cb4231ae005adc1d1d5112154263914ca619
zacaik <646319370@qq.com>
2023-02-09 23:55:41 +0800

    feat: 模块化原理

--- 05-webpack模块化原理/build/js/bundle.js
+++ 05-webpack模块化原理/build/js/bundle.js
@@ -1,60 +1,86 @@
-/******/ (function() { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+(function () {
+  "use strict";
+  var __webpack_modules__ = {
+    "./src/js/math.js": function (
+      __unused_webpack_module,
+      __webpack_exports__,
+      __webpack_require__
+    ) {
+      __webpack_require__.r(__webpack_exports__);
+      __webpack_require__.d(__webpack_exports__, {
+        mul: function () {
+          return mul;
+        },
+        sum: function () {
+          return sum;
+        },
+      });
+      const sum = (num1, num2) => {
+        return num1 + num2;
+      };
 
-/***/ "./src/js/formate.js":
-/*!***************************!*\
-  !*** ./src/js/formate.js ***!
-  \***************************/
-/***/ (function(module) {
+      const mul = (num1, num2) => {
+        return num1 * num2;
+      };
+    },
+  };
+  var __webpack_module_cache__ = {};
 
-const dateFormate = (date) => {
-    return "2022-2-15";
-}
+  function __webpack_require__(moduleId) {
+    var cachedModule = __webpack_module_cache__[moduleId];
+    if (cachedModule !== undefined) {
+      return cachedModule.exports;
+    }
+    var module = (__webpack_module_cache__[moduleId] = {
+      exports: {},
+    });
 
-module.exports = {
-    dateFormate
-}
+    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 
-/***/ })
+    return module.exports;
+  }
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-!function() {
-/*!*****************************!*\
-  !*** ./src/common_index.js ***!
-  \*****************************/
-const { dateFormate } = __webpack_require__(/*! ./js/formate */ "./src/js/formate.js");
+  !(function () {
+    __webpack_require__.d = function (exports, definition) {
+      for (var key in definition) {
+        if (
+          __webpack_require__.o(definition, key) &&
+          !__webpack_require__.o(exports, key)
+        ) {
+          console.log(definition[key]);
+          Object.defineProperty(exports, key, {
+            enumerable: true,
+            get: definition[key], // get属性是一个方法，在对key取值时，会调用get方法
+          });
+        }
+      }
+      console.log(exports);
+    };
+  })();
 
-console.log(dateFormate());
+  !(function () {
+    __webpack_require__.o = function (obj, prop) {
+      return Object.prototype.hasOwnProperty.call(obj, prop);
+    };
+  })();
 
-}();
-/******/ })()
-;
-//# sourceMappingURL=bundle.js.map
+  !(function () {
+    __webpack_require__.r = function (exports) {
+      if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+        Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+      }
+      Object.defineProperty(exports, "__esModule", { value: true });
+    };
+  })();
+
+  var __webpack_exports__ = {};
+  !(function () {
+    __webpack_require__.r(__webpack_exports__);
+    var _js_math_js__WEBPACK_IMPORTED_MODULE_0__ =
+      __webpack_require__("./src/js/math.js");
+
+    console.log(_js_math_js__WEBPACK_IMPORTED_MODULE_0__.sum);
+    console.log((0, _js_math_js__WEBPACK_IMPORTED_MODULE_0__.sum)(10, 20));
+    console.log((0, _js_math_js__WEBPACK_IMPORTED_MODULE_0__.mul)(10, 20));
+  })();
+})();
