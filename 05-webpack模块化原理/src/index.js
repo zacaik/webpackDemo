f8a48372c67a2391fa97d35d9224c4257427898a
zacaik <646319370@qq.com>
2023-02-16 23:48:50 +0800

    source-map初识

--- 05-webpack模块化原理/src/index.js
+++ 05-webpack模块化原理/src/index.js
@@ -2,6 +2,8 @@ const math = require("./js/math.js");
 
 import format from "./js/formate";
 
+console.log(aaa);
+
 console.log(math.sum(10, 20));
 console.log(math.mul(10, 20));
 console.log(format.dateFormate());
