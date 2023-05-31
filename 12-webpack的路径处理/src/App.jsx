7bc74388f328f9141b205d45a36ea80598b5c738
lijunran <646319370@qq.com>
2023-05-31 23:41:53 +0800

    devServer路径,proxy&resolve

---
+++ 12-webpack的路径处理/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <div>App 12312</div>;
+}
---
+++ 12-webpack的路径处理/src/math.js
@@ -0,0 +1,5 @@
+export function sum(num1, num2) {
+  console.log(num1 + num2);
+}
+
+console.log("hello math 11 22");
